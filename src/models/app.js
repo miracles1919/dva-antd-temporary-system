3c5b2dc5ea8651587004acf0de8cfaf62dff8d19
miracles <miracles@fanfandata.com>
2018-04-08 22:24:46 +0800

    增加余额更新

--- src/models/app.js
+++ src/models/app.js
@@ -66,7 +66,7 @@ export default {
       } else if (type === 'shop') {
         authority = ['1', '1-1', '3', '3-1', '3-2']
       } else if (type === 'user') {
-        authority = ['1', '1-1', '3', '3-3']
+        authority = ['1', '1-1', '3', '3-3', '3-4', '3-5']
       }
 
       const menu = menuKeyList.filter((item) => {
@@ -149,6 +149,13 @@ export default {
       return {
         ...state,
         ...navOpenKeys,
+      }
+    },
+
+    updateBalance (state, { payload }) {
+      return {
+        ...state,
+        user: { ...state.user, ...payload },
       }
     },
   },
